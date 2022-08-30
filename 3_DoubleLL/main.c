0afc52b449f9845338e44a49a94b291dede5d4f3
Priyankar Koley <priyankarkoley@gmail.com>
2022-08-30 18:31:36 +0530

    DLL Complete

--- 3_DoubleLL/main.c
+++ 3_DoubleLL/main.c
@@ -20,47 +20,48 @@ OR, 12. EXIT
 #include <stdio.h>
 #include <stdlib.h>
 
-typedef struct list{
+typedef struct list
+{
     int x;
-    struct list* prev;
-    struct list* next;
+    struct list *prev;
+    struct list *next;
 } dll;
 
 char ch;
-dll* head;
+dll *head;
 
-void create(dll*);
-void insert(dll*);
-    void insertAtBeginning(dll*);
-    void insertAtEnd(dll*);
-    void insertAfterAnyPos(dll*);
-void delete_(dll*);
-    void deleteAtBeginning(dll*);
-    void deleteAtEnd(dll*);
-    void deleteAtAnyPos(dll*);
-void view(dll*);
-
+void create(dll *);
+void insert(dll *);
+void insertAtBeginning(dll *);
+void insertAtEnd(dll *);
+void insertAfterAnyPos(dll *);
+void delete_(dll *);
+void deleteAtBeginning(dll *);
+void deleteAtEnd(dll *);
+void deleteAtAnyPos(dll *);
+void view(dll *);
 
 void main()
 {
-	int choice;
-	head =(dll*)malloc(sizeof(dll));
-	head->prev = NULL;
-	while(1)
+    int choice;
+    head = (dll *)malloc(sizeof(dll));
+    head->prev = NULL;
+    while (1)
     {
         printf("\n\n\n\n1. Create a Double Linked List\n2. Insert a NODE \n3. Delete a NODE\n4. View the list\n5. Exit");
         printf("\n\nEnter Choice :");
-		scanf("%d", &choice);
+        scanf("%d", &choice);
 
-		switch (choice) {
-		case 1:
-			create(head);
-			break;
-		case 2:
-			insert(head);
-			break;
-		case 3:
-			delete_(head);
+        switch (choice)
+        {
+        case 1:
+            create(head);
+            break;
+        case 2:
+            insert(head);
+            break;
+        case 3:
+            delete_(head);
             break;
         case 4:
             view(head);
@@ -68,69 +69,68 @@ void main()
         case 5:
             exit(0);
             break;
-		default:
-			printf("Incorrect Choice\n");
-		}
-	}
+        default:
+            printf("Incorrect Choice\n");
+        }
+    }
 }
 
-
-void create(dll* ptr)
+void create(dll *ptr)
 {
     printf("Enter the value: ");
     scanf("%d", &ptr->x);
-    ptr->next=NULL;
+    ptr->next = NULL;
     printf("Do you want to continue?");
     scanf(" %c", &ch);
-    while(ch=='y'||ch=='Y')
+    while (ch == 'y' || ch == 'Y')
     {
-        ptr->next=(dll*)malloc(sizeof(dll));
+        ptr->next = (dll *)malloc(sizeof(dll));
         ptr->next->prev = ptr;
-        ptr=ptr->next;
+        ptr = ptr->next;
         create(ptr);
     }
 }
 
-void view(dll* ptr)
+void view(dll *ptr)
 {
-    while(ptr->next!=NULL)
+    while (ptr->next != NULL)
     {
         printf("\n%d", ptr->x);
-        ptr=ptr->next;
+        ptr = ptr->next;
     }
-    while(ptr!=NULL)
+    while (ptr != NULL)
     {
         printf("\n%d", ptr->x);
-        ptr=ptr->prev;
+        ptr = ptr->prev;
     }
 }
 
-void insert(dll*ptr)
+void insert(dll *ptr)
 {
     int choice;
     printf("\n\n1. Insert a NODE at the beginning\n2. Insert a NODE at the end\n3. Insert a NODE after any element");
     printf("\n\nEnter Choice :");
-		scanf("%d", &choice);
+    scanf("%d", &choice);
 
-		switch (choice) {
-		case 1:
-			insertAtBeginning(ptr);
-			break;
-		case 2:
-			insertAtEnd(ptr);
-			break;
-        case 3:
-    		insertAfterAnyPos(ptr);
-	    	break;
-        default:
-			printf("Incorrect Choice\n");
-
+    switch (choice)
+    {
+    case 1:
+        insertAtBeginning(ptr);
+        break;
+    case 2:
+        insertAtEnd(ptr);
+        break;
+    case 3:
+        insertAfterAnyPos(ptr);
+        break;
+    default:
+        printf("Incorrect Choice\n");
     }
 }
 
-void insertAtBeginning(dll* ptr)
+void insertAtBeginning(dll *ptr)
 {
-    dll* new = (dll*)malloc(sizeof(dll));
+    dll *new = (dll *)malloc(sizeof(dll));
     printf("Enter the value: ");
     scanf("%d", &new->x);
     new->prev = head->prev;
@@ -139,47 +139,48 @@ void insertAtBeginning(dll* ptr)
     head = new;
 }
 
-void insertAtEnd(dll* ptr)
+void insertAtEnd(dll *ptr)
 {
-    dll* new = (dll*)malloc(sizeof(dll));
+    dll *new = (dll *)malloc(sizeof(dll));
     printf("Enter the value: ");
     scanf("%d", &new->x);
-    while(ptr->next!=NULL)
+    while (ptr->next != NULL)
         ptr = ptr->next;
     new->next = ptr->next;
     new->prev = ptr;
     ptr->next = new;
 }
 
-void insertAfterAnyPos(dll* ptr)
+void insertAfterAnyPos(dll *ptr)
 {
     int ele;
     printf("Enter the element to insert after: ");
     scanf("%d", &ele);
-    while(ptr->next!=NULL)
+    while (ptr->next != NULL)
     {
-        if(ptr->x == ele)
+        if (ptr->x == ele)
         {
-            dll* new = (dll*)malloc(sizeof(dll));
+            dll *new = (dll *)malloc(sizeof(dll));
             printf("Enter the value: ");
             scanf("%d", &new->x);
             new->next = ptr->next;
-            new->prev=ptr;
-            ptr->next->prev=new;
-            ptr->next=new;
+            new->prev = ptr;
+            ptr->next->prev = new;
+            ptr->next = new;
         }
         ptr = ptr->next;
     }
 }
 
-void delete_(dll* ptr)
+void delete_(dll *ptr)
 {
     int choice;
     printf("\n\n1. Delete a NODE at the beginning\n2. Delete a NODE at the end\n3. Delete any element");
     printf("\n\nEnter Choice :");
     scanf("%d", &choice);
 
-    switch (choice) {
+    switch (choice)
+    {
     case 1:
         deleteAtBeginning(ptr);
         break;
@@ -194,19 +195,19 @@ void delete_(dll* ptr)
     }
 }
 
-void deleteAtBeginning(dll* ptr)
+void deleteAtBeginning(dll *ptr)
 {
-    dll* node = ptr;
+    dll *node = ptr;
     head = ptr->next;
     head->prev = NULL;
     free(node);
 }
 
-void deleteAtEnd(dll* ptr)
+void deleteAtEnd(dll *ptr)
 {
-    dll* preptr;
-    dll* node;
-    while(ptr->next!=NULL)
+    dll *preptr;
+    dll *node;
+    while (ptr->next != NULL)
     {
         preptr = ptr;
         ptr = ptr->next;
@@ -216,21 +217,21 @@ void deleteAtEnd(dll* ptr)
     free(node);
 }
 
-void deleteAtAnyPos(dll* ptr)
+void deleteAtAnyPos(dll *ptr)
 {
     int ele;
-    dll* node;
-    dll* preptr=ptr;
+    dll *node;
+    dll *preptr = ptr;
     printf("Enter the value to be deleted: ");
     scanf("%d", &ele);
-    while(ptr!=NULL)
+    while (ptr != NULL)
     {
-        if(ptr->x == ele)
+        if (ptr->x == ele)
         {
             preptr->next = ptr->next;
             ptr->next->prev = ptr->prev;
             node = ptr;
-            ptr=ptr->next;
+            ptr = ptr->next;
             free(node);
         }
         preptr = ptr;
